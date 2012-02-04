ba2745d0ab94dc6850b36dea9dcf3f06654c80ee
Ian Young <ian.greenleaf@gmail.com>
2012-02-04 00:16:49 -0800

    Test page, script to dynamically insert content

---
+++ bad-ie.js
@@ -0,0 +1,4 @@
+$(function() {
+  var fragment = "<div id='ie_upgrade_message'> <h1>Your browser is letting you down.</h1> <div class='explanation'>Switch to a different browser and experience a safer, more beautiful Internet.</div> <ul class='suggested_browsers'> <li class='firefox'> <a href='https://www.mozilla.com/en-US/firefox/new/'>Firefox</a> </li> <li class='chrome'> <a href='http://www.google.com/chrome'>Chrome</a> </li> </ul> </div> ";
+  $("body").prepend($(fragment));
+});
